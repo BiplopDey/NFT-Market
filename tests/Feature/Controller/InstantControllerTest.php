e0be2f945e4b8f5c20100570ae0198455cb2a66a
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2021-12-17 18:41:10 +0100

    add test

---
+++ tests/Feature/Controller/InstantControllerTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Tests\Feature\Controller;
+
+use App\Models\Instant;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class InstantControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+    // CREATE
+    public function test_auth_user_can_see_an_instant_create_form()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('instants.create'));//acting as user loged in
+
+        $response->assertStatus(200)->assertViewIs('instantForm');//check if is rendering the instantForm.blade.php
+    }
+
+    public function test_not_auth_user_cannot_see_an_instant_create_form_and_redirect_to_login()
+    {
+        
+        $response = $this->get(route('instants.create'));//acting as user loged in
+
+        $response->assertStatus(302)->assertRedirect('/login');//the middleware always redirect
+    }
+
+    public function test_auth_user_can_create_an_instant()
+    {
+        $user = User::factory()->create();
+        $data = [
+            'title' => 'titulo1',
+            'img' => 'http://hola.jpg'
+        ];
+        $response = $this->actingAs($user)->post(route('instants.store'), $data);
+
+        $this->assertDatabaseCount('instants', 1);
+    }
+
+    public function test_not_auth_user_cant_create_an_instant()
+    {
+        $user = User::factory()->create();
+        $data = [
+            'title' => 'titulo1',
+            'img' => 'http://hola.jpg'
+        ];
+        $response = $this->post(route('instants.store'), $data);
+
+        $this->assertDatabaseCount('instants', 0);
+    }
+
+    //DELETE
+    public function test_not_auth_user_cant_delete_an_instant()
+    {
+        User::factory()->create();
+        $instant = Instant::factory()->create();
+        
+        $response = $this->delete(route('instants.delete',$instant->id));
+
+        $this->assertDatabaseCount('instants', 1);
+    }
+
+    public function test_auth_user_can_delete_own_instant()
+    {
+        $user = User::factory()->create();
+        $instant = Instant::factory()->create();
+        
+        $response = $this->actingAs($user)->delete(route('instants.delete',$instant->id));
+
+        $this->assertDatabaseCount('instants', 0);
+    }
+
+    public function test_auth_user_cant_delete_others_instant()
+    {
+        $user = User::factory()->create();
+        $instant = Instant::factory()->create(['user_id'=>2]);
+        
+        $response = $this->actingAs($user)->delete(route('instants.delete',$instant->id));
+
+        $this->assertDatabaseCount('instants', 1);
+    }
+
+
+}
--- tests/Feature/Controller/LandingControllerTest.php
+++ tests/Feature/Controller/LandingControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\Controller;
 
+use App\Models\User;
+use App\Models\Instant;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -16,8 +18,29 @@ class LandingControllerTest extends TestCase
      */
     public function test_can_see_a_landig_page()
     {
-        $response = $this->get('/');
+        $response = $this->get(route('landing'));
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)->assertViewIs('landing');
+    }
+
+    public function test_can_see_all_user_instant_list()
+    {
+        User::factory(3)->create();
+        $instants = Instant::factory(3)->create();
+        
+        $response = $this->get(route('landing'));
+        $response->assertSee($instants[0]->title);// looks at html and find the given string
+        //$response->assertStatus(200)->assertViewIs('landing');
+    }
+
+    public function test_can_see_uthors_name_in_instant_list()
+    {
+        $user=User::factory()->create();
+        $instants = Instant::factory(3)->create();
+        
+        $response = $this->get(route('landing'));
+        $response->assertSee($instants[0]->author->title)
+        ->assertSee($instants[1]->author->title)
+        ->assertSee($user->name);
     }
 }
